f5e9eea2482a539235cd79acdcfb561a5a8f8ffc
Konrad-Le <lechkonrad@o2.pl>
2024-05-04 06:02:29 +0200

    first commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+    
+    let a = 0;
+   
+    function changeBackgroundColor () {
+    document.body.style.backgroundColor = getRandomHexColor();
+    a = setTimeout(changeBackgroundColor, 1000);
+} ;
+
+
+document.querySelector("[data-start]").addEventListener('click',function(){
+    this.disabled = true;
+    document.querySelector("[data-stop]").disabled =false;
+    changeBackgroundColor();
+
+});
+
+document.querySelector("[data-stop]").addEventListener('click',function(){
+    this.disabled = true;
+    document.querySelector("[data-start]").disabled =false;
+    changeBackgroundColor.disabled;
+    clearTimeout(a);
+});
+
+// koniec
